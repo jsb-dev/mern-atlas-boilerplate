f086bc2e552463c20832d1dc54d636c57bcc69c6
jsb-dev <jsb-dev@outlook.com>
2023-03-12 16:49:39 +1100

    Added database configuration

---
+++ server/database/database.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const connection = mongoose.connection;
+connection.on('error', console.error.bind(console, 'connection error:'));
+connection.once('open', () => {
+  console.log('Connected to MongoDB database');
+});
+
+export default connection;
--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import connection from './database/database.js';
 import connectRouter from './api/routers/connect.js';
 
 dotenv.config();
 
 // Express
 const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 app.use(express.json());
 
 // CORS
